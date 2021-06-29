5f3ee4be9dddffbb3f6f43464e0a61df7b7c9c10
Amir Orbe <orbebrian@gmail.com>
2021-06-29 14:17:10 -0500

    solucion de un error de sintaxi

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -27,7 +27,7 @@ defmodule RumblWeb.UserController do
             |> put_flash(:info, "#{user.name} created!")
             |> redirect(to: Routes.user_path(conn, :index))
             {:error, %Ecto.Changeset{} = changeset} ->
-                render(conn, "new.html", changeset)
+                render(conn, "new.html", changeset: changeset)
         end
     end
 end
